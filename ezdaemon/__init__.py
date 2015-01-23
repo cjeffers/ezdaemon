3ed8c10a2ea029f7a89839c4c8b345e4edd54cdb
cjeffers <cwjeffers18@gmail.com>
2015-01-23 14:13:42 -0700

    Add everything to empty repo

---
+++ ezdaemon/__init__.py
@@ -0,0 +1,19 @@
+"""Daemonize makes Unix-y daemons real easy. Just import daemonize.daemonize
+and call the function before whatever you want the daemon to be. A couple
+gotchas:
+
+    1) It will disconnect your python process from stdin and stdout, so any
+    print calls will not show up. This is because daemons are disconnected
+    from any controlling terminal.
+
+    2) Similarly, the working directory is changed to the root folder. This
+    is to prevent lockup in case any virtual volumes are unmounted. Just make
+    sure any IO uses the absolute path.
+"""
+
+from daemon import daemonize
+
+
+if __name__ == "__main__":
+    assert daemonize
+    print "imported ok"
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='ezdaemon',
+    version='0.1',
+    packages=['ezdaemon'],
+    license='MIT',
+    long_description=open('README.md').read()
+)
