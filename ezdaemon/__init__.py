6fab7a8170cbd993400b097478f328024c3f9247
cjeffers <cwjeffers18@gmail.com>
2015-01-23 14:30:02 -0700

    Make init docstring reflect README

--- ezdaemon/__init__.py
+++ ezdaemon/__init__.py
@@ -1,14 +1,26 @@
-"""Daemonize makes Unix-y daemons real easy. Just import daemonize.daemonize
-and call the function before whatever you want the daemon to be. A couple
-gotchas:
+"""ezdaemon makes Unix-y daemons real easy. Just import ezdaemon.daemonize
+and call it before whatever you want the daemon to be. A couple gotchas:
 
-    1) It will disconnect your python process from stdin and stdout, so any
-    print calls will not show up. This is because daemons are disconnected
-    from any controlling terminal.
+1. It will disconnect your python process from stdin and stdout, so any
+   print calls will not show up. This is because daemons are disconnected
+   from any controlling terminal.
+2. Similarly, the working directory is changed to the root folder. This
+   is to prevent lockup in case any virtual volumes are unmounted. Just make
+   sure any IO uses the absolute path.
 
-    2) Similarly, the working directory is changed to the root folder. This
-    is to prevent lockup in case any virtual volumes are unmounted. Just make
-    sure any IO uses the absolute path.
+Example:
+
+    from ezdaemon import daemonize
+
+    def do_daemony_stuff():
+        # your code here
+        logfile = open('/some/absolute/path.log').read()  # IO needs abspath
+
+    if __name__ == "__main__":
+        print "before daemon"  # this will print
+        daemonize()
+        print "in daemon"  # this will go to /dev/null (aka not print)
+        do_daemony_stuff()
 """
 
 from daemon import daemonize
@@ -16,4 +28,4 @@ from daemon import daemonize
 
 if __name__ == "__main__":
     assert daemonize
-    print "imported ok"
+    print "ezdaemon.daemonize imported ok"
